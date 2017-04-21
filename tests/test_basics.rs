31f7fdb7224aecdcbe5b17cc6bc0415d256e8f31
Your Name <bbeloshitskyy@gmail.com>
2017-04-21 15:17:13 +0300

    ticket#2 add test for overflow and for zero sum

--- tests/test_basics.rs
+++ tests/test_basics.rs
@@ -9,3 +9,15 @@ fn check_positive_sum() {
 fn check_negative_sun(){
     assert_eq!(-5, debt_tools::add(-2, -3))
 }
+
+#[test]
+fn check_zero_sum(){
+    assert_eq!(0, debt_tools::add(0, 0))
+}
+
+#[test]
+#[should_panic]
+fn check_overflow(){
+    let max_val = i32::max_value();
+    debt_tools::add(max_val, 1);
+}
